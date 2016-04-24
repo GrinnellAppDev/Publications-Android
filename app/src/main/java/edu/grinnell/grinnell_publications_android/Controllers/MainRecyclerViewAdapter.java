7f06abba9da5dab26237d5e141c9e70c64aeed39
Prabir Pradhan <prabirmsp@gmail.com>
2016-04-24 14:48:29 -0500

    create new recycler view adapter class

---
+++ app/src/main/java/edu/grinnell/grinnell_publications_android/Controllers/MainRecyclerViewAdapter.java
@@ -0,0 +1,24 @@
+package edu.grinnell.grinnell_publications_android.Controllers;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by prabir on 4/24/16, AppDev Grinnell.
+ */
+public class MainRecyclerViewAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/edu/grinnell/grinnell_publications_android/Controllers/RecyclerViewHolder.java
@@ -0,0 +1,16 @@
+package edu.grinnell.grinnell_publications_android.Controllers;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by prabir on 4/24/16, AppDev Grinnell.
+ */
+public class RecyclerViewHolder extends RecyclerView.ViewHolder{
+
+
+
+    public RecyclerViewHolder(int type, View itemView) {
+        super(itemView);
+    }
+}
