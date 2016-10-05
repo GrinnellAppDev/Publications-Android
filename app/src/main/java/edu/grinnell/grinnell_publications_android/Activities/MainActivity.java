f98c5c473b0f6a1ccc7e9d1b7425b5c869422dc4
murphymatt <murphyma@grinnell.edu>
2016-10-04 22:25:45 -0500

    removed buildsearch function added searchable.xml

--- app/src/main/java/edu/grinnell/grinnell_publications_android/Activities/MainActivity.java
+++ app/src/main/java/edu/grinnell/grinnell_publications_android/Activities/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
@@ -109,11 +110,6 @@ public class MainActivity extends AppCompatActivity implements UserInterface {
                 R.string.drawer_close);
         drawerLayout.addDrawerListener(drawerToggle);
         drawerToggle.syncState();
-        buildSearch();
-    }
-
-    public void buildSearch() {
-
     }
 
     private void replaceFrameWithFragment(Fragment fragment){
