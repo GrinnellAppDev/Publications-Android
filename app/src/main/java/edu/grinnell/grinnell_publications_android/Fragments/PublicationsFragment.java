dffa19d1055994b75a918115f0d5bda28ca84a07
murphymatt <murphyma@grinnell.edu>
2016-10-04 22:05:29 -0500

    added buildSearchBar function to PublicationsFragment

--- app/src/main/java/edu/grinnell/grinnell_publications_android/Fragments/PublicationsFragment.java
+++ app/src/main/java/edu/grinnell/grinnell_publications_android/Fragments/PublicationsFragment.java
@@ -31,6 +31,10 @@ public class PublicationsFragment extends Fragment implements UserInterface{
 
     @Override
     public void initializeUI() {
+        buildSearchBar();
+    }
 
+    public void buildSearchBar() {
+        
     }
 }
