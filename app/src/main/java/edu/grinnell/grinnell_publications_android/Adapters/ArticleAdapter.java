d8f0633f71a1c4542fa46cf7aed7778a6bb2eb6f
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2016-12-04 13:26:50 -0600

    added adapter for articles in profile

---
+++ app/src/main/java/edu/grinnell/grinnell_publications_android/Adapters/ArticleAdapter.java
@@ -0,0 +1,50 @@
+package edu.grinnell.grinnell_publications_android.Adapters;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import edu.grinnell.grinnell_publications_android.Models.Realm.RealmStory;
+import edu.grinnell.grinnell_publications_android.R;
+
+/**
+ * Created by Mattori on 12/3/16.
+ */
+
+public class ArticleAdapter extends RecyclerView.Adapter {
+    private List<RealmStory> stories;
+
+    public static class ArticleViewHolder extends RecyclerView.ViewHolder {
+        public CardView card;
+
+        public ArticleViewHolder(View view) {
+            super(view);
+        }
+    }
+
+    public ArticleAdapter(List<RealmStory> stories) {
+        this.stories = stories;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                               .inflate(R.layout.viewholder_article, parent, false);
+        ArticleViewHolder vh = new ArticleViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        RealmStory story = stories.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return stories.size();
+    }
+}
