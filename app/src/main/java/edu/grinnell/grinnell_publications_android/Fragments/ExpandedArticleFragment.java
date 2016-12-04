03dcdc71cd21e0b66d22bc57eeaf8320c2f4d265
yazankittaneh <yazanakittaneh@gmail.com>
2016-12-03 21:29:31 -0600

    Clean up code for PR

--- app/src/main/java/edu/grinnell/grinnell_publications_android/Fragments/ExpandedArticleFragment.java
+++ app/src/main/java/edu/grinnell/grinnell_publications_android/Fragments/ExpandedArticleFragment.java
@@ -1,22 +1,18 @@
 package edu.grinnell.grinnell_publications_android.Fragments;
 
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
+
 import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
-import android.support.design.widget.AppBarLayout;
+
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
-import android.util.DisplayMetrics;
 
-import android.support.v7.widget.Toolbar;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -27,7 +23,6 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.squareup.picasso.Picasso;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -35,17 +30,12 @@ import edu.grinnell.grinnell_publications_android.Models.Interfaces.UserInterfac
 import edu.grinnell.grinnell_publications_android.R;
 
 
-
-import java.util.Date;
-import java.util.logging.StreamHandler;
-
 /**
  * Fragment that displays the expanded article content.
  * Fragment allows for favoriting of the article.
  */
 
 public class ExpandedArticleFragment extends Fragment implements UserInterface {
-    @Bind(R.id.article_fragment_toolbar) Toolbar mToolbar;
     @Bind(R.id.header_image) ImageView mHeaderImage;
     @Bind(R.id.collapsing_toolbar) CollapsingToolbarLayout mCollapsingToolbar;
     @Bind(R.id.floating_action_button) FloatingActionButton mFavoriteButton;
@@ -55,7 +45,6 @@ public class ExpandedArticleFragment extends Fragment implements UserInterface {
 
     public static ExpandedArticleFragment newInstance() {
         ExpandedArticleFragment expandedArticleFragment = new ExpandedArticleFragment();
-        expandedArticleFragment.setHasOptionsMenu(true);
         return expandedArticleFragment;
     }
 
@@ -63,41 +52,26 @@ public class ExpandedArticleFragment extends Fragment implements UserInterface {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mToolbar.inflateMenu(R.menu.toolbar_settings_menu);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
-        Log.d("Oncreateview", "Inside the method");
         final View expandedArticleFragment = inflater.inflate(R.layout.fragment_extended_article, container, false);
         ButterKnife.bind(this, expandedArticleFragment);
-        setHasOptionsMenu(true);
         initializeUI();
         return expandedArticleFragment;
     }
 
     @Override
     public void initializeUI(){
-        mToolbar.setNavigationIcon(ContextCompat.getDrawable(getContext(), R.drawable.ic_action_back));
-        mToolbar.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.scarlet_red));
-        mCollapsingToolbar.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.scarlet_red));
-        mToolbar.inflateMenu(R.menu.toolbar_settings_menu);
         loadPlaceHolderData();
         setOnClickListeners();
     }
 
 
     public void setOnClickListeners() {
-        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getFragmentManager().popBackStack();
-            }
-        });
-
-
         mFavoriteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -106,28 +80,6 @@ public class ExpandedArticleFragment extends Fragment implements UserInterface {
         });
     }
 
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        String TAG = "onCreateOptionsMenu: ";
-        Log.d(TAG, "Inside the method");
-        inflater.inflate(R.menu.toolbar_settings_menu, menu);
-        Log.d(TAG, "after inflate");
-        super.onCreateOptionsMenu(menu, inflater);
-        Log.d(TAG, "Called super method");
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        String TAG = "onOptionsItemSelected: ";
-        Log.d(TAG, "Inside the method");
-        switch(item.getItemId()) {
-            case R.id.article_settings:
-                Log.d(TAG, "Switch activated");
-                Snackbar.make(getView(), "Setting button pressed", Snackbar.LENGTH_SHORT).show();
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
 
     /** Fills fragment with placeholder content */
     private void loadPlaceHolderData(){
