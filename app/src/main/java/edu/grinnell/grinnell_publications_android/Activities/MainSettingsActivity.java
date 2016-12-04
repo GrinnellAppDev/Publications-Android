3d4a329e46c9ee4396f7407872662537136252f9
murphymatt <murphyma@grinnell.edu>
2016-12-03 20:23:11 -0600

    Joint Commit (Matt + Larry) : * remove instances of butterknife * manually bind layouts * update butterknife dependency

--- app/src/main/java/edu/grinnell/grinnell_publications_android/Activities/MainSettingsActivity.java
+++ app/src/main/java/edu/grinnell/grinnell_publications_android/Activities/MainSettingsActivity.java
@@ -1,11 +1,9 @@
 package edu.grinnell.grinnell_publications_android.Activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
 import edu.grinnell.grinnell_publications_android.Models.Interfaces.UserInterface;
 import edu.grinnell.grinnell_publications_android.R;
 
@@ -15,12 +13,14 @@ import edu.grinnell.grinnell_publications_android.R;
  */
 public class MainSettingsActivity extends AppCompatActivity implements UserInterface {
 
-    @Bind(R.id.settings_toolbar)Toolbar toolbar;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        ButterKnife.bind(this);
+
+        toolbar = (Toolbar)findViewById(R.id.settings_toolbar);
+
         initializeUI();
     }
 
