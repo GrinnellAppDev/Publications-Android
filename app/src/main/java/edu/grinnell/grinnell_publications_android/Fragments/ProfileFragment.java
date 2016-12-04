79dcf6f58be7cc143878ebe032bd67b3aaff727a
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2016-12-04 12:13:12 -0600

    began implementing the CoordinatorLayout and RecyclerView for profile

--- app/src/main/java/edu/grinnell/grinnell_publications_android/Fragments/ProfileFragment.java
+++ app/src/main/java/edu/grinnell/grinnell_publications_android/Fragments/ProfileFragment.java
@@ -3,10 +3,13 @@ package edu.grinnell.grinnell_publications_android.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import edu.grinnell.grinnell_publications_android.Adapters.ArticleAdapter;
 import edu.grinnell.grinnell_publications_android.Models.Interfaces.UserInterface;
 import edu.grinnell.grinnell_publications_android.R;
 
@@ -24,12 +27,20 @@ public class ProfileFragment extends Fragment implements UserInterface {
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
-        initializeUI();
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        initializeUI(view);
+        return view;
     }
 
     @Override
-    public void initializeUI() {
+    public void initializeUI(View view) {
+        initRecyclerView((RecyclerView) view.findViewById(R.id.articles_recyclerview));
+    }
 
+    private void initRecyclerView(RecyclerView rv) {
+        RecyclerView.LayoutManager lm = new LinearLayoutManager(getContext());
+        rv.setLayoutManager(lm);
+
+        rv.setAdapter(new ArticleAdapter());
     }
 }
