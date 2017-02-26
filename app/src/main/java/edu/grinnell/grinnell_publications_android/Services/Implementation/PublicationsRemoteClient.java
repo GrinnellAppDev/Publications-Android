cc8f857c91ab3ed57847436e6a63bb372222dd2e
grinnelldennis <chanchun@grinnell.edu>
2017-02-26 13:49:52 -0600

    establish aws connection, converting response json objects into realm publications and stories

--- app/src/main/java/edu/grinnell/grinnell_publications_android/Services/Implementation/PublicationsRemoteClient.java
+++ app/src/main/java/edu/grinnell/grinnell_publications_android/Services/Implementation/PublicationsRemoteClient.java
@@ -1,28 +1,22 @@
 package edu.grinnell.grinnell_publications_android.Services.Implementation;
 
 import android.util.Log;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
+import android.widget.Toast;
 import edu.grinnell.grinnell_publications_android.Models.Interfaces.Publication;
-import edu.grinnell.grinnell_publications_android.Models.Interfaces.Story;
 import edu.grinnell.grinnell_publications_android.Models.Realm.RealmAuthor;
+import edu.grinnell.grinnell_publications_android.Models.Realm.RealmAuthorContact;
 import edu.grinnell.grinnell_publications_android.Models.Realm.RealmPublication;
 import edu.grinnell.grinnell_publications_android.Models.Realm.RealmStory;
 import edu.grinnell.grinnell_publications_android.Models.RemoteQueryResponse;
 import edu.grinnell.grinnell_publications_android.Services.Interfaces.LocalClientAPI;
 import edu.grinnell.grinnell_publications_android.Services.Interfaces.RemoteClientAPI;
+import edu.grinnell.grinnell_publications_android.Services.POJO.JAuthor;
 import edu.grinnell.grinnell_publications_android.Services.POJO.JPublication;
 import edu.grinnell.grinnell_publications_android.Services.POJO.JStory;
-import edu.grinnell.grinnell_publications_android.Services.POJO.JAuthor;
 import io.realm.Realm;
 import io.realm.RealmList;
-import io.realm.internal.IOException;
+import java.util.List;
+import java.util.Set;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -37,124 +31,112 @@ import retrofit2.http.Path;
  * <p> All networking calls are made through calling on a retrofit networking service.
  * As an observable, updates to the remote calls are routed to the respective observers for either
  * changes to the UI or changes to the local data base.</p>
+ *
  * @author Albert Owusu-Asare
  * @version 1.1 Thu May  5 15:57:45 CDT 2016
  */
-    public class PublicationsRemoteClient implements RemoteClientAPI {
+public class PublicationsRemoteClient implements RemoteClientAPI {
 
-    private Realm realm;
-    private Retrofit retrofit;
-    private LocalClientAPI localClient;
-    private AWSEndpointApiInterface api;
-    private final String BASE_API = "https://g2j7qs2xs7.execute-api.us-west-2.amazonaws.com/devstable/"; //TODO: Get base URL
-    
-    public PublicationsRemoteClient(){
-        this(new RealmLocalClient());
-    }
+  private Realm realm;
+  private Retrofit retrofit;
+  private LocalClientAPI localClient;
+  private AWSEndpointApiInterface api;
+  private final String BASE_API =
+      "https://g2j7qs2xs7.execute-api.us-west-2.amazonaws.com/devstable/";
 
-    public PublicationsRemoteClient(LocalClientAPI localClient){
-        this.localClient = localClient;
-        
-        retrofit = new Retrofit.Builder().baseUrl(BASE_API)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        api = retrofit.create(AWSEndpointApiInterface.class);
+  public PublicationsRemoteClient() {
+    this(new RealmLocalClient());
+  }
 
-        realm = Realm.getDefaultInstance();
-        realm.beginTransaction();
-    }
+  public PublicationsRemoteClient(LocalClientAPI localClient) {
+    this.localClient = localClient;
 
-    @Override
-    public void getAllPublications() {
-        Call<List<JPublication>> call = api.publications();
-        call.enqueue(new Callback<List<JPublication>>() {
-            @Override
-            public void onResponse(Call<List<JPublication>> call, Response<List<JPublication>> response) {
-                Log.d("TAG", "Success");
-                storePublication(response.body());
-            }
-            public void onFailure(Call<List<JPublication>> call, Throwable t) {
-                Log.d("TAG", "Failure");
-            }
-        });
-    }
-    private void storePublication(List<JPublication> list) {
-        for (JPublication item : list) {
-            RealmPublication pub = new RealmPublication(item.getName(), item.getId(),
-                    null, null, null);
-            realm.copyToRealm(pub);
-            realm.commitTransaction();
-        }
-    }
+    retrofit = new Retrofit.Builder().baseUrl(BASE_API)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build();
+    api = retrofit.create(AWSEndpointApiInterface.class);
 
-    public void getStory(String publicationId, String articleId) {
-        Call<JStory> call = api.article(publicationId, articleId);
-        call.enqueue(new Callback<JStory>() {
-            @Override
-            public void onResponse(Call<JStory> call, Response<JStory> response) {
-                Log.d("TAG", "Success: getStory");
-                storeStory(convertIntoRealmStory(response.body()));
-            }
-            public void onFailure(Call<JStory> call, Throwable t) {
-                Log.d("TAG", "Failure: getStory");
-            }
-        });
-    }
+    realm = Realm.getDefaultInstance();
+    realm.beginTransaction();
+  }
+
+  @Override public void getAllPublications() {
+    Call<List<JPublication>> call = api.publications();
+    call.enqueue(new Callback<List<JPublication>>() {
+      @Override
+      public void onResponse(Call<List<JPublication>> call, Response<List<JPublication>> response) {
+        storeRealmPublication(response.body());
+      }
+
+      public void onFailure(Call<List<JPublication>> call, Throwable t) {
+        Toast.makeText(getActivity(), "Failed to get all publication!", Toast.LENGTH_LONG).show();
+      }
+    });
+  }
+
+  public void getStory(String publicationId, String articleId) {
+    Call<JStory> call = api.article(publicationId, articleId);
+    call.enqueue(new Callback<JStory>() {
+      @Override public void onResponse(Call<JStory> call, Response<JStory> response) {
+        storeRealmStory(convertRealmStory(response.body()));
+      }
+      public void onFailure(Call<JStory> call, Throwable t) {
+        Toast.makeText(getActivity(), "Failed to get story!", Toast.LENGTH_LONG).show();
+      }
+    });
+  }
 
     /* Private helper methods */
-
-    private RealmStory convertIntoRealmStory (JStory story) {
-        RealmList realmAuthorList = new RealmList<RealmAuthor>();
-        for (JAuthor author : story.getAuthors()) {
-            RealmAuthorContact contact = new RealmAuthorContact(author.getEmail(), null, null);
-            realmAuthorList.add(new RealmAuthor(author.getName(), contact, null. null ))
-        }
-        RealmStory realmStory = new RealmStory(null, story.getDatePublished(), story.getDateEdited(),
-                realmAuthorList, null, story.getContent(), story.getbrief(), story.getTitle(), 
-                story.getHeaderImage());
-        return realmStory;
+  private RealmStory convertRealmStory(JStory story) {
+    RealmList realmAuthorList = new RealmList<RealmAuthor>();
+    for (JAuthor author : story.getAuthors()) {
+      RealmAuthorContact contact = new RealmAuthorContact(author.getEmail(), null, null);
+      realmAuthorList.add(new RealmAuthor(author.getName(), contact, null, null));
     }
+    RealmStory realmStory =
+        new RealmStory(null, story.getDatePublished(), story.getDateEdited(), realmAuthorList, null,
+            story.getContent(), story.getBrief(), story.getTitle(), story.getHeaderImage());
+    return realmStory;
+  }
 
-    private void storeRealmStory(RealmStory realmStory) {
-        // TODO: store realm story into realm objects
-        //>get & return list of publications from realm
+  private void storeRealmStory(RealmStory realmStory) {
+    // TODO: store realm story into realm objects
+    //>get & return list of publications from realm
+    //>update realm publication with story
+  }
 
-        //>update realm publication with story
-
+  private void storeRealmPublication(List<JPublication> list) {
+    for (JPublication item : list) {
+      RealmPublication pub = new RealmPublication(item.getName(), item.getId(), null, null, null);
+      realm.copyToRealm(pub);
+      realm.commitTransaction();
     }
+  }
 
-    //NOT SUPPORTED BY DATABASE PEOPLE YET 
-    public void getPublication(String id) { }
-   @Override
-    public void getPublications(Set<Integer> publicationIds) { }
+  /* Not currently supported by database people/AWS endpoints*/
+  @Override public void getPublications(Set<Integer> publicationIds) { }
 
+  public void getPublication(String id) { }
 
-    /* Public methods */
-
-    // TODO : Add logging to the HTTP calls.
-    // TODO : DRY out service calls.? Repetition
+  // TODO : Add logging to the HTTP calls.
+  // TODO : DRY out service calls.? Repetition
     /* Private classes  and interfaces */
-    private interface AWSEndpointApiInterface {
-        /* Publications */
-        @GET("publications/")
-        Call<List<JPublication>> publications();
-        /* Articles */
-        @GET("publications/{publicationId}/articles/{articleId}")
-        Call<JStory> article(
-            @Path("publicationId")String publicationId,
-            @Path("articleId")String articleId);
+  private interface AWSEndpointApiInterface {
+    /* Publications */
+    @GET("publications/") Call<List<JPublication>> publications();
 
-        /* Not supported database */
+    /* Articles */
+    @GET("publications/{publicationId}/articles/{articleId}") Call<JStory> article(
+        @Path("publicationId") String publicationId, @Path("articleId") String articleId);
+
+    /* Not currently supported by database people/AWS */
         /* Publications by id */
-        @GET("publications/{id}")
-        Call<Publication> publication( @Path("id")String publicationId);
-        /* Images */
-        @GET("images/{id}")
-        Call<RemoteQueryResponse> image( @Path("id")int imageId);
-        /* Series */
-        @GET("series/{id}")
-        Call<RemoteQueryResponse> series( @Path("id")int seriesId);
+    @GET("publications/{id}") Call<Publication> publication(@Path("id") String publicationId);
 
+    /* Images */
+    @GET("images/{id}") Call<RemoteQueryResponse> image(@Path("id") int imageId);
 
-    }
+    /* Series */
+    @GET("series/{id}") Call<RemoteQueryResponse> series(@Path("id") int seriesId);
+  }
 }
