43c5586b8ac9567563fef378546ec1a5efa2b16d
Jemuel Santos <santosje@grinnell.edu>
2017-03-01 20:29:47 -0600

    fixed issues that were found with constants, readability, variable naming, and lack of comments

--- app/src/main/java/edu/grinnell/grinnell_publications_android/CONSTANTS.java
+++ app/src/main/java/edu/grinnell/grinnell_publications_android/CONSTANTS.java
@@ -7,8 +7,9 @@ package edu.grinnell.grinnell_publications_android;
  */
 
 public class CONSTANTS {
-    public static final String PUBLICATIONID ="publicationId";
-    public static final String LASTUPDATED ="lastUpdated";
-    public static final String STORYID ="storyId";
+    public static final String PUBLICATION_ID="publicationId";
+    public static final String LAST_UPDATED ="lastUpdated";
+    public static final String STORY_ID ="storyId";
+    public static final int LIST_START = 0;
 
 }
