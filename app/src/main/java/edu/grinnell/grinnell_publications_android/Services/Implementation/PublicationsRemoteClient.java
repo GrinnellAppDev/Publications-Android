8f8b4e23d3389a63871bdb8a596d53cd552510dd
yazankittaneh <yazanakittaneh@gmail.com>
2017-03-02 20:48:40 -0600

    my changes

--- app/src/main/java/edu/grinnell/grinnell_publications_android/Services/Implementation/PublicationsRemoteClient.java
+++ app/src/main/java/edu/grinnell/grinnell_publications_android/Services/Implementation/PublicationsRemoteClient.java
@@ -22,65 +22,62 @@ import retrofit2.http.Path;
  * <p> All networking calls are made through calling on a retrofit networking service.
  * As an observable, updates to the remote calls are routed to the respective observers for either
  * changes to the UI or changes to the local data base.</p>
+ *
  * @author Albert Owusu-Asare
  * @version 1.1 Thu May  5 15:57:45 CDT 2016
  */
 public class PublicationsRemoteClient implements RemoteClientAPI {
-    private Retrofit retrofit;
-    private LocalClientAPI localClient;
-    private final PublicationsServiceAPI publicationsService;
-    private final String BASE_API = "Insert Base API URL"; //TODO: Get base URL
+  private Retrofit retrofit;
+  private LocalClientAPI localClient;
+  private final PublicationsServiceAPI publicationsService;
+  private final String BASE_API = "Insert Base API URL"; //TODO: Get base URL
 
+  public PublicationsRemoteClient() {
+    this(new RealmLocalClient());
+  }
 
-    public PublicationsRemoteClient(){
-        this(new RealmLocalClient());
-    }
+  public PublicationsRemoteClient(LocalClientAPI localClient) {
+    this.localClient = localClient;
+    this.retrofit = initializeRetrofitRestAdapter();
+    this.publicationsService = this.retrofit.create(PublicationsServiceAPI.class);
+  }
 
-    public PublicationsRemoteClient(LocalClientAPI localClient){
-        this.localClient = localClient;
-        this.retrofit = initializeRetrofitRestAdapter();
-        this.publicationsService = this.retrofit.create(PublicationsServiceAPI.class);
-    }
+  @Override public List<Publication> getAllPublications() {
+    return null;
+  }
 
-    @Override
-    public List<Publication> getAllPublications() {
-        return null;
-    }
+  @Override public Publication getPublication(int id) {
+    return null;
+  }
 
-    @Override
-    public Publication getPublication(int id) {
-        return null;
-    }
-
-    @Override
-    public List<Publication> getPublications(Set<Integer> publicationIds) {
-        return null;
-    }
+  @Override public List<Publication> getPublications(Set<Integer> publicationIds) {
+    return null;
+  }
 
     /* Public methods */
 
-
-    //TODO : Add logging to the HTTP calls.
-    // TODO : DRY out service calls.? Repetition
+  //TODO : Add logging to the HTTP calls.
+  // TODO : DRY out service calls.? Repetition
     /* Private classes  and interfaces */
-    private interface PublicationsServiceAPI {
-        /* Publications*/
-        @GET("publications/{id}")
-        Call<RemoteQueryResponse> publication( @Path("id")int publicationId);
-        /* Articles */
-        @GET("articles/{id}")
-        Call<RemoteQueryResponse> article( @Path("id")int articleId);
-        /* Images */
-        @GET("images/{id}")
-        Call<RemoteQueryResponse> image( @Path("id")int imageId);
-        /* Series */
-        @GET("series/{id}")
-        Call<RemoteQueryResponse> series( @Path("id")int seriesId);
-    }
-    /* Private helper methods */
-    private Retrofit initializeRetrofitRestAdapter() {
-        Gson gson = new GsonBuilder().create();
-        return new Retrofit.Builder()
-                .baseUrl(BASE_API).addConverterFactory(GsonConverterFactory.create(gson)).build();
-    }
+  private interface PublicationsServiceAPI {
+    /* Publications*/
+    @GET("publications/{id}") Call<RemoteQueryResponse> publication(@Path("id") int publicationId);
+
+    /* Articles */
+    @GET("articles/{id}") Call<RemoteQueryResponse> article(@Path("id") int articleId);
+
+    /* Images */
+    @GET("images/{id}") Call<RemoteQueryResponse> image(@Path("id") int imageId);
+
+    /* Series */
+    @GET("series/{id}") Call<RemoteQueryResponse> series(@Path("id") int seriesId);
+  }
+
+  /* Private helper methods */
+  private Retrofit initializeRetrofitRestAdapter() {
+    Gson gson = new GsonBuilder().create();
+    return new Retrofit.Builder().baseUrl(BASE_API)
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build();
+  }
 }
--- app/src/main/java/edu/grinnell/grinnell_publications_android/Services/Implementation/RealmLocalClient.java
+++ app/src/main/java/edu/grinnell/grinnell_publications_android/Services/Implementation/RealmLocalClient.java
@@ -23,124 +23,111 @@ import io.realm.RealmResults;
  * @see io.realm.Realm
  */
 public class RealmLocalClient implements LocalClientAPI {
-    @Override
-    public boolean isCacheEmpty() {
-        return false;
+  @Override public boolean isCacheEmpty() {
+    return false;
+  }
+
+  @Override public boolean isCacheFull() {
+    return false;
+  }
+
+  @Override public void addToSubscribedPublications(int publicationId) {
+
+  }
+
+  @Override public void savePublications(List<Publication> publications) {
+    Realm realm = Realm.getDefaultInstance();
+    realm.beginTransaction();
+
+    for (int i = 0; i < publications.size(); i++) {
+      realm.copyToRealmOrUpdate((RealmPublication) publications.get(i));
     }
+    realm.commitTransaction();
+  }
 
-    @Override
-    public boolean isCacheFull() {
-        return false;
+  @Override public void savePublication(Publication publication) {
+    Realm realm = Realm.getDefaultInstance();
+    realm.beginTransaction();
+
+    RealmResults<RealmPublication> pubList = realm.where(RealmPublication.class)
+        .equalTo(CONSTANTS.PUBLICATIONID, publication.getPublicationId())
+        .findAll();
+
+    if (pubList.size() == 0) {
+      realm.copyToRealm((RealmPublication) publication);
     }
+    realm.commitTransaction();
+  }
 
-    @Override
-    public void addToSubscribedPublications(int publicationId) {
+  @Override public List<Publication> getSubscribedPublications(int id) {
+    return null;
+  }
 
+  @Override public List<Publication> getAllPublications() {
+    Realm realm = Realm.getDefaultInstance();
+    RealmResults<RealmPublication> pubResults = realm.where(RealmPublication.class).findAll();
+    List<Publication> mResults = new ArrayList<>();
+    mResults.addAll(pubResults.subList(0, pubResults.size()));
+    return mResults;
+  }
+
+  @Override public List<Story> getAllStories(int page, int numStoriesPerPage) {
+    Realm realm = Realm.getDefaultInstance();
+    RealmResults<RealmStory> mRealmResults = realm.where(RealmStory.class).findAll();
+    List<Story> mResults = new ArrayList<>();
+    mResults.addAll(mRealmResults.subList(0, mRealmResults.size())); //puts realmResults in a list
+    return mResults;
+  }
+
+  @Override public List<Story> getRecentStories(List<Integer> subscribedPublicationIds,
+      Date mostRecentStory) {
+    Realm realm = Realm.getDefaultInstance();
+    List<Story> allStories = new ArrayList<>();
+
+    for (Integer currentPublicationId : subscribedPublicationIds) { //for each subscribed publication
+      RealmResults<RealmStory> publicationStories = realm.where(RealmStory.class)
+          .equalTo(CONSTANTS.PUBLICATIONID, currentPublicationId)
+          .greaterThan(CONSTANTS.LASTUPDATED, mostRecentStory)
+          .findAll();
+      allStories.addAll(
+          publicationStories.subList(0, publicationStories.size())); //puts realmResults in a list
     }
+    //TODO: sort all stories by date published
+    return allStories;
+  }
 
-    @Override
-    public void savePublications(List<Publication> publications) {
-        Realm realm = Realm.getDefaultInstance();
-        realm.beginTransaction();
+  @Override public Story getFullStoryById(int storyId) {
+    Realm realm = Realm.getDefaultInstance();
+    return realm.where(RealmStory.class).equalTo(CONSTANTS.STORYID, storyId).findFirst();
+  }
 
-        for(int i = 0; i < publications.size(); i++) {
-            realm.copyToRealmOrUpdate((RealmPublication)publications.get(i));
-        }
-        realm.commitTransaction();
-    }
+  @Override public List<Story> getAllStoriesInSeries(int publicationId, int seriesId, int page,
+      int numStoriesPerPage) {
+    return null;
+  }
 
-    @Override
-    public void savePublication(Publication publication) {
-        Realm realm = Realm.getDefaultInstance();
-        realm.beginTransaction();
+  @Override public List<Story> getRecentStories(int seriesId, Date mostRecentStoryInSeries) {
+    Realm realm = Realm.getDefaultInstance();
+    RealmResults<RealmStory> mRealmResults = realm.where(RealmStory.class)
+        .greaterThan(CONSTANTS.LASTUPDATED, mostRecentStoryInSeries)
+        .findAll();
+    List<Story> mResults = new ArrayList<>();
+    mResults.addAll(mRealmResults.subList(0, mRealmResults.size())); //puts realmResults in a list
+    return mResults;
+  }
 
-        RealmResults<RealmPublication> pubList = realm.where(RealmPublication.class)
-                .equalTo(CONSTANTS.PUBLICATIONID, publication.getPublicationId())
-                .findAll();
+  @Override public List<Story> getBookmarkedStories() {
+    return new ArrayList<>();
+  }
 
-        if(pubList.size() == 0) {
-            realm.copyToRealm((RealmPublication) publication);
-        }
-        realm.commitTransaction();
-    }
+  @Override public User getProfile() {
+    return null;
+  }
 
-    @Override
-    public List<Publication> getSubscribedPublications(int id) {
-        return null;
-    }
-
-    @Override
-    public List<Publication> getAllPublications() {
-        Realm realm = Realm.getDefaultInstance();
-        RealmResults<RealmPublication> pubResults = realm.where(RealmPublication.class).findAll();
-        List<Publication> mResults = new ArrayList<>();
-        mResults.addAll(pubResults.subList(0, pubResults.size()));
-        return mResults;
-    }
-
-    @Override
-    public List<Story> getAllStories(int page, int numStoriesPerPage) {
-        Realm realm = Realm.getDefaultInstance();
-        RealmResults<RealmStory> mRealmResults= realm.where(RealmStory.class).findAll();
-        List<Story> mResults = new ArrayList<>();
-        mResults.addAll(mRealmResults.subList(0, mRealmResults.size())); //puts realmResults in a list
-        return mResults;
-    }
-
-    @Override
-    public List<Story> getRecentStories(List<Integer> subscribedPublicationIds, Date mostRecentStory) {
-        Realm realm = Realm.getDefaultInstance();
-        List<Story> allStories = new ArrayList<>();
-
-        for (Integer currentPublicationId: subscribedPublicationIds) { //for each subscribed publication
-            RealmResults<RealmStory> publicationStories = realm
-                    .where(RealmStory.class)
-                    .equalTo(CONSTANTS.PUBLICATIONID, currentPublicationId)
-                    .greaterThan(CONSTANTS.LASTUPDATED, mostRecentStory)
-                    .findAll();
-            allStories.addAll(publicationStories.subList(0, publicationStories.size())); //puts realmResults in a list
-        }
-        //TODO: sort all stories by date published
-        return allStories;
-    }
-
-    @Override
-    public Story getFullStoryById(int storyId) {
-        Realm realm = Realm.getDefaultInstance();
-        return realm.where(RealmStory.class).equalTo(CONSTANTS.STORYID, storyId).findFirst();
-    }
-
-    @Override
-    public List<Story> getAllStoriesInSeries(int publicationId, int seriesId, int page, int numStoriesPerPage) {
-        return null;
-    }
-
-    @Override
-    public List<Story> getRecentStories(int seriesId, Date mostRecentStoryInSeries) {
-        Realm realm = Realm.getDefaultInstance();
-        RealmResults<RealmStory> mRealmResults= realm.where(RealmStory.class)
-                .greaterThan(CONSTANTS.LASTUPDATED, mostRecentStoryInSeries)
-                .findAll();
-        List<Story> mResults = new ArrayList<>();
-        mResults.addAll(mRealmResults.subList(0, mRealmResults.size())); //puts realmResults in a list
-        return mResults;
-    }
-
-    @Override
-    public List<Story> getBookmarkedStories() {
-        return new ArrayList<>();
-    }
-
-    @Override
-    public User getProfile() {
-        return null;
-    }
-
-    @Override
-    public Publication getPublicationById(String publicationId) {
-        Realm realm = Realm.getDefaultInstance();
-        return realm.where(RealmPublication.class)
-                .equalTo(CONSTANTS.PUBLICATIONID, publicationId)
-                .findFirst();
-    }
+  @Override public Publication getPublicationById(String publicationId) {
+    Realm realm = Realm.getDefaultInstance();
+    return realm.where(RealmPublication.class)
+        .equalTo(CONSTANTS.PUBLICATIONID, publicationId)
+        .findFirst();
+  }
 }
