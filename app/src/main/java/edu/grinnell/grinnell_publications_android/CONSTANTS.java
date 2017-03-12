942ffbcda2ba0476dacb35bf3b442bc2899ed317
grinnelldennis <chanchun@grinnell.edu>
2017-03-12 15:02:33 -0500

    added aws_base_api into constraints

--- app/src/main/java/edu/grinnell/grinnell_publications_android/CONSTANTS.java
+++ app/src/main/java/edu/grinnell/grinnell_publications_android/CONSTANTS.java
@@ -7,7 +7,10 @@ package edu.grinnell.grinnell_publications_android;
  */
 
 public class Constants {
-    public static final String PUBLICATION_ID="publicationId";
+
+    public static final String AWS_BASE_API =
+        "https://g2j7qs2xs7.execute-api.us-west-2.amazonaws.com/devstable/";
+    public static final String PUBLICATION_ID ="publicationId";
     public static final String LAST_UPDATED ="lastUpdated";
     public static final String STORY_ID ="storyId";
     public static final int LIST_START = 0;
