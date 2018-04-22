d81649ded8526da93ac9b3dc77c36ac57f2d2282
Ankit Pandey <pandeyan@grinnell.edu>
2018-04-22 14:54:27 -0500

    Use appdev.grinnell.edu for API

--- app/src/main/java/edu/grinnell/grinnell_publications_android/Constants.java
+++ app/src/main/java/edu/grinnell/grinnell_publications_android/Constants.java
@@ -8,7 +8,7 @@ package edu.grinnell.grinnell_publications_android;
 
 public class Constants {
     public static final String AWS_BASE_API =
-            "https://g2j7qs2xs7.execute-api.us-west-2.amazonaws.com/devstable/";
+            "https://appdev.grinnell.edu/api/publications/v1/";
     public static final String PUBLICATION_ID ="publicationId";
     public static final String LAST_UPDATED ="lastUpdated";
     public static final String STORY_ID ="storyId";
